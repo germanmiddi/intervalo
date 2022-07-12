f1b27ef195669c452469cbd2d52659b6eac7f5f9
Guille <guillermojoaquingarciaibiris@gmail.com>
2022-07-11 21:06:52 -0300

    CardExpandBase

--- src/resources/js/Layouts/Web/CompetenciaItem.vue
+++ src/resources/js/Layouts/Web/CompetenciaItem.vue
@@ -1,24 +1,77 @@
 <template>
-     <div class="card bg-base-100 shadow-xl transition duration-300 ease-in"
-         :class="{'bg-blue-50' : this.selected == true,
-                    'col-span-2' : this.expand   == true,
-                    'col-span-1' : this.expand   == false}" >
-        <div class="card-body">
-            <h2 class="card-title text-academy-accent h-12">{{item.competencia}}</h2>
-            <p class="h-16 ">{{item.resume}}</p>
-            <div class="card-actions justify-end">
-                <!-- <a @click="this.expand = !this.expand"> Más</a>
+  <div
+    class="card bg-base-100 shadow-xl transition duration-300 ease-in"
+    :class="{
+      'bg-blue-50': this.selected == true,
+      'col-span-2': this.expand == true,
+      'col-span-1': this.expand == false,
+    }"
+  >
+    <div class="card-body">
+      <h2 class="card-title text-academy-accent h-12">
+        {{ item.competencia }}
+      </h2>
+
+      <p v-if="!item.visible" class="h-24">
+        {{ item.resume.slice(0, 150) }}...
+      </p>
+      <p v-else class="h-24 overflow-y-hidden">{{ item.resume }}</p>
+      <button
+        class="mt-2"
+        @click="(item.visible = !item.visible), toggleText()"
+      >
+        <span v-if="!item.visible" class="font-bold ml-4">Leer mas</span>
+        <span v-else class="font-bold ml-4">Ocultar</span>
+      </button>
+
+      <div class="flex justify-between">
+        <div class="card-actions justify-end w-full">
+          <!-- <a @click="this.expand = !this.expand"> Más</a>
                 <a @click="this.expand = !this.expand"> Menos</a> -->
-                <button v-show="!this.selected" @click="this.selected = !this.selected" class="btn btn-primary space-x-2 mt-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span>Seleccionar</span>
-                </button>
-                <button v-show="this.selected" @click="this.selected = !this.selected" class="btn space-x-2 mt-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>                    
-                    <span>Deseleccionar</span>
-                </button>
-            </div>
-            <!-- <div v-show="this.expand">
+          <button
+            v-show="!this.selected"
+            @click="this.selected = !this.selected"
+            class="btn btn-primary space-x-2 mt-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="h-6 w-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span>Seleccionar</span>
+          </button>
+          <button
+            v-show="this.selected"
+            @click="this.selected = !this.selected"
+            class="btn space-x-2 mt-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Deseleccionar</span>
+          </button>
+        </div>
+      </div>
+      <!-- <div v-show="this.expand">
                 <div>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas laudantium perferendis officiis non sed nesciunt unde exercitationem suscipit sunt? Cum sed voluptatum fugiat illo incidunt! Quaerat vel dolor debitis facere.
                     Labore vel harum accusamus nemo ducimus quam consequuntur eos! Officiis consequuntur incidunt voluptatum maxime, aliquid fugit quas reprehenderit, magnam, neque cumque quis! Reiciendis in ab similique dolore. Tempore, saepe veniam?
@@ -27,25 +80,29 @@
                     Commodi, quidem omnis recusandae nam ipsam odit architecto sapiente iusto alias, eligendi et sint minima! Fugiat, dolorum sit. Distinctio adipisci deserunt iste magni eos necessitatibus modi natus eaque in! Recusandae.
                 </div>
             </div> -->
-        </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props:{
-            item: Object,
-            index: String
-        },
-        data(){
-            return{
-                selected: false,
-                expand: false
-            }
-        }
-    }
+export default {
+  props: {
+    item: Object,
+    index: String,
+  },
+  data() {
+    return {
+      selected: false,
+      expand: false,
+    };
+  },
+  methods: {
+    toggleText() {
+      this.$emit("toggleText", this.index);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
